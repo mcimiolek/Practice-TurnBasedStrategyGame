f8f798214aed679611bcad22ac7b0ebb35841e9d
MatthewImiolek <mcimiolek@gmail.com>
2020-08-11 14:17:08 -0500

    finished first version of TileSet abstact class

--- src/main/java/MatthewImiolek/games/Strategy/TileSet.java
+++ src/main/java/MatthewImiolek/games/Strategy/TileSet.java
@@ -5,27 +5,66 @@ package MatthewImiolek.games.Strategy;
    as the one it is next to. This is then extended to create the actual tile sets. */
 
 public abstract class TileSet {
-    String baseTile;
-    double maxPlainChance;
-    double minPlainChance;
-    double maxWaterChance;
-    double minWaterChance;
-    double maxForestChance;
-    double minForestChance;
-    double maxMountainChance;
-    double minMountainChance;
-    double contChancePlain;
-    double contChanceWater;
-    double contChanceForest;
-    double contChanceMountain;
+    String baseTile;            // The base tile type for the set, which are then changed to other tiles
+    double maxPlainRange;       // Max percent of the board that will be plains
+    double minPlainRange;       // Min percent of the board that will be plains
+    double maxWaterRange;       // Max percent of the board that will be water
+    double minWaterRange;       // Min percent of the board that will be water
+    double maxForestRange;      // Max percent of the board that will be forest
+    double minForestRange;      // Min percent of the board that will be forest
+    double maxMountainRange;    // Max percent of the board that will be mountains
+    double minMountainRange;    // Min percent of the board that will be mountains
+    double contChancePlain;     // Additional chance of a plain next to a plain
+    double contChanceWater;     // Additional chance of a water next to a water
+    double contChanceForest;    // Additional chance of a forest next to a forest
+    double contChanceMountain;  // Additional chance of a mountain next to a mountain
 
-    public void setBaseTile(){}            // Sets the default tile type for a set, which will be the base until replaced
-    public void setChanceRangeForest(){}   // Sets percent range for amount of forest tiles
-    public void setChanceRangeWater(){}    // Sets percent range for amount water tiles
-    public void setChanceRangePlain(){}    // Sets percent range for amount of plains tiles
-    public void setChanceRangeMountain(){} // Sets percent range for amount of mountain tiles
-    public void setContChanceForest(){}    // Sets the additional chance for a forest next to another forest tile
-    public void setContChanceWater(){}     // Sets the additional chance for a water next to another water tile
-    public void setContChancePlain(){}     // Sets the additional chance for a plain next to another plain tile
-    public void setContChanceMountain(){}  // Sets the additional chance for a mountain next to another mountain tile
+    // Sets the default tile type for a set, which will be then be replaced with other tile types
+    public void setBaseTile(String tileType){
+        this.baseTile = tileType;
+    }
+
+    // Sets percent range for amount of forest tiles
+    public void setChanceRangeForest(double max, double min){
+        this.maxForestRange = max;
+        this.minForestRange = min;
+    }
+
+    // Sets percent range for amount water tiles
+    public void setChanceRangeWater(double max, double min){
+        this.maxWaterRange = max;
+        this.minWaterRange = min;
+    }
+
+    // Sets percent range for amount of plains tiles
+    public void setChanceRangePlain(double max, double min){
+        this.maxPlainRange = max;
+        this.minPlainRange = min;
+    }
+
+    // Sets percent range for amount of mountain tiles
+    public void setChanceRangeMountain(double max, double min){
+        this.maxMountainRange = max;
+        this.minMountainRange = min;
+    }
+
+    // Sets the additional chance for a forest next to another forest tile
+    public void setContChanceForest(double contChanceForest){
+        this.contChanceForest = contChanceForest;
+    }
+
+    // Sets the additional chance for a water next to another water tile
+    public void setContChanceWater(double contChanceWater){
+        this.contChanceWater = contChanceWater;
+    }
+
+    // Sets the additional chance for a plain next to another plain tile
+    public void setContChancePlain(double contChancePlain){
+        this.contChancePlain = contChancePlain;
+    }
+
+    // Sets the additional chance for a mountain next to another mountain tile
+    public void setContChanceMountain(double contChanceMountain){
+        this.contChanceMountain = contChanceMountain;
+    }
 }

b973e4702178c47f6b42ada1e599379194dd46b6
MatthewImiolek <mcimiolek@gmail.com>
2020-08-19 16:04:39 -0500

    Added radio buttons to select board size

--- src/main/java/MatthewImiolek/games/Strategy/BoardGenerator.java
+++ src/main/java/MatthewImiolek/games/Strategy/BoardGenerator.java
@@ -21,6 +21,7 @@ public class BoardGenerator {
         background.setVisible(true);
         background.setSize(screenSize.width, screenSize.height);
         background.setLocation(0, 0);
+        background.setDefaultCloseOperation(background.EXIT_ON_CLOSE);
 
         return background;
     }
@@ -28,24 +29,36 @@ public class BoardGenerator {
     // Create a popup where the user selects the board options
     public JFrame boardChoices(){
         JFrame setup = new JFrame("Board choices");
+        GridBagConstraints setupLayout = new GridBagConstraints();
+        ButtonGroup boardSize = new ButtonGroup();
 
         setup.setVisible(true);
         setup.setSize(250,250);
         setup.setLocation(100,100);
+        setup.setLayout(new GridBagLayout());
 
-        ButtonGroup boardSize = new ButtonGroup();
+        setupLayout.anchor = GridBagConstraints.LINE_START;
 
         JRadioButton small = new JRadioButton("Small");
+        setupLayout.gridx = 0;
+        setupLayout.gridy = 0;
+        small.setSelected(true);
+        setup.add(small, setupLayout);
+
+
         JRadioButton medium = new JRadioButton("Medium");
+        setupLayout.gridx = 0;
+        setupLayout.gridy = 1;
+        setup.add(medium, setupLayout);
+
         JRadioButton large = new JRadioButton("Large");
+        setupLayout.gridx = 0;
+        setupLayout.gridy = 2;
+        setup.add(large, setupLayout);
 
         boardSize.add(small);
         boardSize.add(medium);
         boardSize.add(large);
-
-        JComponent newContentPane = new RadioButtonDemo();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
 
         return setup;
     }

29b32ed79d0367094adf30af829f45cf5bd02f1d
MatthewImiolek <mcimiolek@gmail.com>
2020-11-27 11:52:18 +0100

    Updating minor things

--- src/main/java/MatthewImiolek/games/Strategy/BackgroundVisuals.java
+++ src/main/java/MatthewImiolek/games/Strategy/BackgroundVisuals.java
@@ -31,18 +31,18 @@ public class BackgroundVisuals extends JFrame {
     public void addTiles(String boardSize, String tileSetName) {
         TileSetStringMap setStringMap = new TileSetStringMap();
         SizeStringArrayPairs sizePairs = new SizeStringArrayPairs();
-        int[] boardSizePair = sizePairs.getPairArray(boardSize);                                        // A pair containing the height and width of the board
+        int[] boardSizePair = sizePairs.getPairArray(boardSize);                                                // A pair containing the height and width of the board
         tiles = new Tile[boardSizePair[0]][boardSizePair[1]];
         double baseHeight = (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 100) / boardSizePair[1]; // Height of each background
-        double baseWidth = Toolkit.getDefaultToolkit().getScreenSize().getWidth() / boardSizePair[0];   // Width of each background
-        double borderWidth = baseHeight / 40;
-        double tileHeight = baseHeight - (2 * borderWidth);
-        double tileWidth = baseWidth - (2 * borderWidth);
-        TileSet tileSet = setStringMap.getTileSet(tileSetName);                                         // Create the tile set for the board
-        int lastTile = tileSet.getBaseTile();                                                           // The last tile so tile chances can be changed, defaulted to the base tile
-        double[] baseTileChances = tileSet.calcTileChance();                                            // The chance of each tile type by default
-        double[] updatedChances = baseTileChances.clone();                                              // The chance of each tile after accounting for previous tile
-        double randNum;                                                                                 // A random number selecting the next tile
+        double baseWidth = baseHeight;                                                                          // Width of each background
+        double borderWidth = baseHeight / 40;                                                                   // The amount of border around each tile
+        double tileHeight = baseHeight - (2 * borderWidth);                                                     // The width of each tile
+        double tileWidth = tileHeight;                                                                          // The height of each tile
+        TileSet tileSet = setStringMap.getTileSet(tileSetName);                                                 // Create the tile set for the board
+        int lastTile = tileSet.getBaseTile();                                                                   // The last tile so tile chances can be changed, defaulted to the base tile
+        double[] baseTileChances = tileSet.calcTileChance();                                                    // The chance of each tile type by default
+        double[] updatedChances = baseTileChances.clone();                                                      // The chance of each tile after accounting for previous tile
+        double randNum;                                                                                         // A random number selecting the next tile
         Random rand = new Random();
 
         // Add tiles to array and to the content pane
@@ -56,24 +56,32 @@ public class BackgroundVisuals extends JFrame {
                     tiles[x][y].setFilled(true);
                     tiles[x][y].setFillColor(tiles[x][y].getColor());
                     content.add(tiles[x][y]);
+
+                    lastTile = 0;
                 }
                 else if(randNum < updatedChances[1]) {
                     tiles[x][y] = new ForestTile((x * baseWidth) + borderWidth, (y * baseHeight) + borderWidth, tileWidth, tileHeight);
                     tiles[x][y].setFilled(true);
                     tiles[x][y].setFillColor(tiles[x][y].getColor());
                     content.add(tiles[x][y]);
+
+                    lastTile = 1;
                 }
                 else if (randNum < updatedChances[2]) {
                     tiles[x][y] = new MountainTile((x * baseWidth) + borderWidth, (y * baseHeight) + borderWidth, tileWidth, tileHeight);
                     tiles[x][y].setFilled(true);
                     tiles[x][y].setFillColor(tiles[x][y].getColor());
                     content.add(tiles[x][y]);
+
+                    lastTile = 2;
                 }
                 else {
                     tiles[x][y] = new WaterTile((x * baseWidth) + borderWidth, (y * baseHeight) + borderWidth, tileWidth, tileHeight);
                     tiles[x][y].setFilled(true);
                     tiles[x][y].setFillColor(tiles[x][y].getColor());
                     content.add(tiles[x][y]);
+
+                    lastTile = 3;
                 }
             }
         }

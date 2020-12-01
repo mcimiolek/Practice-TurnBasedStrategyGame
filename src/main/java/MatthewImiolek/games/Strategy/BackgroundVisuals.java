8f28cdd71545d54b026b353f96ed133e99e2b322
MatthewImiolek <mcimiolek@gmail.com>
2020-12-01 23:49:00 +0100

    Updated textbox sizeing

--- src/main/java/MatthewImiolek/games/Strategy/BackgroundVisuals.java
+++ src/main/java/MatthewImiolek/games/Strategy/BackgroundVisuals.java
@@ -8,9 +8,13 @@ import java.util.*;
 
 // Class that actually makes the board
 public class BackgroundVisuals extends JFrame {
-    GCanvas content = new GCanvas();    // Shows all the tiles
-    Tile[][] tiles;                     // Create a tile array to hold each tile
-    JTextArea text;                     // Area that displays text
+    GCanvas content = new GCanvas();                                               // Shows all the tiles
+    Tile[][] tiles;                                                                // Create a tile array to hold each tile
+    JTextArea text;                                                                // Area that displays text
+    double tileEnd = 0.0;                                                          // Right side of the tiles
+    double screenHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight(); // The height of the screen
+    double screenWidth = Toolkit.getDefaultToolkit().getScreenSize().getWidth();   // The width of the screen
+    double borderWidth = 0.0;                                                      // Size of the border to go around everything
 
     // Constructor for a new background setup
     BackgroundVisuals(){
@@ -29,21 +33,22 @@ public class BackgroundVisuals extends JFrame {
 
     // Function which adds tiles to the board
     public void addTiles(String boardSize, String tileSetName) {
-        TileSetStringMap setStringMap = new TileSetStringMap();
-        SizeStringArrayPairs sizePairs = new SizeStringArrayPairs();
-        int[] boardSizePair = sizePairs.getPairArray(boardSize);                                                // A pair containing the height and width of the board
+        TileSetStringMap setStringMap = new TileSetStringMap();      // Holds the types of tile sets available
+        SizeStringArrayPairs sizePairs = new SizeStringArrayPairs(); // Holds the number and format of the tile pairs
+        int[] boardSizePair = sizePairs.getPairArray(boardSize);     // A pair containing the height and width of the board
         tiles = new Tile[boardSizePair[0]][boardSizePair[1]];
-        double baseHeight = (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 100) / boardSizePair[1]; // Height of each background
-        double baseWidth = baseHeight;                                                                          // Width of each background
-        double borderWidth = baseHeight / 40;                                                                   // The amount of border around each tile
-        double tileHeight = baseHeight - (2 * borderWidth);                                                     // The width of each tile
-        double tileWidth = tileHeight;                                                                          // The height of each tile
-        TileSet tileSet = setStringMap.getTileSet(tileSetName);                                                 // Create the tile set for the board
-        int lastTile;                                                                                           // The last tile so tile chances can be changed, defaulted to the base tile
-        double[] baseTileChances = tileSet.calcTileChance();                                                    // The chance of each tile type by default
-        double[] updatedChances = baseTileChances.clone();                                                      // The chance of each tile after accounting for previous tile
-        double randNum;                                                                                         // A random number selecting the next tile
-        Random rand = new Random();
+        double baseHeight = (screenHeight - 100) / boardSizePair[1]; // Height of each background
+        double baseWidth = baseHeight;                               // Width of each background
+        borderWidth = baseHeight / 40;
+        double tileHeight = baseHeight - (2 * borderWidth);          // The width of each tile
+        double tileWidth = tileHeight;                               // The height of each tile
+        TileSet tileSet = setStringMap.getTileSet(tileSetName);      // Create the tile set for the board
+        int lastTile;                                                // The last tile so tile chances can be changed, defaulted to the base tile
+        double[] baseTileChances = tileSet.calcTileChance();         // The chance of each tile type by default
+        double[] updatedChances = baseTileChances.clone();           // The chance of each tile after accounting for previous tile
+        double randNum;                                              // A random number selecting the next tile
+        Random rand = new Random();                                  // Random number generator
+        tileEnd = (boardSizePair[0] * baseWidth) + (borderWidth * 5);
 
         // Add tiles to array and to the content pane
         for(int x = 0; x < boardSizePair[0]; x++) {
@@ -90,15 +95,17 @@ public class BackgroundVisuals extends JFrame {
     }
 
     // Add a JTextArea to hold information about the board on
+    //TODO: FIGURE OUT COLUMNS AND ROWS BETTER
     public void addText() {
-        double xPos = Toolkit.getDefaultToolkit().getScreenSize().getHeight(); // The yPosition to avoid tiles
-
         text = new JTextArea();
         text.setEditable(false);
-        text.setLocation((int) xPos,20);
-        text.setColumns(10);
-        text.setRows(10);
+        text.setLocation((int) tileEnd,(int) borderWidth * 5);
+        text.setColumns((int) (screenWidth - tileEnd) / 12);
+        text.setRows((int) (screenHeight - 250) / 14);
         text.setBackground(Color.darkGray);
         content.add(text);
+
+        System.out.println(text.getFont());
+        System.out.println(text.getFont());
     }
 }
